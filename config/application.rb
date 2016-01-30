f112388f514236e500ff6bcce9156598c67ad579
phillip <pmonell@gmail.com>
2016-01-30 13:41:29 -0500

    Add rspec and factory girl to generator config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Ecommerce
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories' 
+
+      g.view_specs false
+      g.helper_specs false
+    end
   end
 end
