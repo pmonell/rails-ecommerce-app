adbfa80f170c72f974302e9c01da3be2891f5987
phillip <pmonell@gmail.com>
2016-01-30 18:55:39 -0500

    Fix scoping issue with rails-api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::API
+require "application_responder"
+
+class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
 end
