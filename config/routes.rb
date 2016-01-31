5bfcc7028dd5d27f069a4496fc40efd9b03e5cbc
phillip <pmonell@gmail.com>
2016-01-30 20:48:26 -0500

    Add create action endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'customer/index', :to => 'customer#index'
+      resources :customers, only: [:index, :show, :create]
     end
   end
 
